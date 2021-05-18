a9250eea99b0f51552999b7a9cbb4920a7820a29
jonaswsilva <jonaswdasilvaa@gmail.com>
2021-05-18 17:51:02 -0300

    primeiras alteracoes

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUpdatePost;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(){
+
+        $posts = Post::get();
+
+        return view('admin.posts.index', compact('posts'));
+    }
+
+    public function create(){
+        return view('admin.posts.create');
+    }
+
+    public function store(StoreUpdatePost $request){
+        
+        Post::create($request->all());
+        
+        return redirect()->route('posts.index');
+    }
+
+}
---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,6 @@
+<a href="{{ route('posts.create') }}">Criar Novo Post</a>
+<hr>
+<h1>Posts</h1>
+@foreach ($posts as $post)
+    <p>{{ $post->title }}</p>
+@endforeach
